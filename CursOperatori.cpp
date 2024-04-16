67ddb5a6b7343bd2253309bd40783159afa6fbc6
Voicu Librimir <voicu.librimir@gmail.com>
2024-04-16 22:11:54 +0300

    Add project files.

---
+++ CursOperatori.cpp
@@ -0,0 +1,163 @@
+// CursOperatori.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+	/*const int anul_curent = 2024;
+	int anul_nasterii;
+	std::cout << "Introudceti anul nasterii :";
+	std::cin >> anul_nasterii;
+
+	int varsta;
+	varsta = anul_curent - anul_nasterii;
+
+	std::cout << "Varsta ta este" << varsta << "de ani" << std::endl;*/
+
+
+
+
+
+
+
+	//std::cout << std::endl;
+
+	//int var1 = 2;
+	//int var2 = 10;
+
+
+	//int suma = var1 + var2;
+	//int scadere = var1 - var2;
+	//int inumultire = var1 * var2;
+	//int impartire = var1 / var2;
+	//int modulo = var1 % var2;//modul ne da restul impartirii ( se poate verifica daca un  numar este par sau nu)
+
+	//nt calcul = var1 + var2 * var2;//ordinea ii var2*var2 si rezultatul se aduna cu var1
+	//int calcul2 = (var1 + var2) * var2;//ordinea ii var1+var2 si rezultatul se aduna cu var2
+
+	// <mai mic
+	// >mai mare 
+	//  <=mai mic sau egal  
+	//  >=mai mare sau egal
+
+	//var1 = 5;
+	//var2 = 10;
+	//bool esteNumarMic = var1 < var2;
+	//bool esteNumarMare = var1 > var2;
+	//bool esteNumarMaiMicSauEgal = var1 <= var2;
+	//bool esteNumarMaiMareSauEgal = var1 >= var2;
+
+	//bool test = -1;
+
+	//std::cout << esteNumarMic << std::endl;
+
+	// == operatorul de comparatie egal
+	// != operatorul de comparatie diferit
+
+	//var1 = 5;
+	//var2 = 10;
+	//bool este_egal = var1 == var2;
+	//bool este_diferit = var1 != var2;
+
+
+	//bool incurcat = este_egal != este_diferit;
+
+
+	// ++ incrementare
+	// -- decrementare
+
+	//var1 = 9;
+	//var1++; //am incrementat var1
+	//std::cout << var1 << std::endl;
+	//ar1--;
+	//std::cout << var1 << std::endl;
+
+	//var2 = 15;
+	//int var3 = var2++; // operator de incrementare postfix
+	//std::cout << var3 << std::endl;
+	//std::cout << var2 << std::endl;
+
+
+	 //var3 = ++var2; // operator de incrementare prefix
+	//std::cout << var3 << std::endl;
+	//std::cout << var2 << std::endl;
+	//int a = 5;
+	//std::cout << ++a << std::endl;
+	//std::cout << a << std::endl;
+
+	// minicalculator: utilizatorul sa introduca 2 variabile si sa printati toate operatiile pe care le stim cum aceste 2 variabile
+
+	 //&& AND SI-ul logc
+	 // || OR SAU-ul Logic
+	 // ! negatie
+	//bool a = true;
+	//bool b = false;
+
+
+	/*std::cout << "a AND b =" << (a && b) << std::endl;
+	std::cout << "b AND a =" << (b && a) << std::endl;
+	std::cout << "a AND a =" << (a && a) << std::endl;
+	std::cout << "b AND b =" << (b && b) << std::endl;*/
+
+	
+	//std::cout << "a OR b =" << (a || b) << std::endl;
+	//std::cout << "b OR a=" << (b|| a) << std::endl;
+	//std::cout << "a OR a=" << (a || a) << std::endl;
+	//std::cout << "b OR b=" << (b || b) << std::endl;
+
+	//bool rezultat = !a; // Not True
+	//std::cout << rezultat << std::endl;
+
+	/*bool test = !rezultat;
+	std::cout << test << std::endl;*/
+
+
+
+	//rezultat = !b; //NOT False
+	//std::cout << rezultat << std::endl;
+
+	//int c = 5;
+	//int d =+ 9;
+	//d += 4; //d = d+ 4;
+	//std::cout << d << std::endl;
+	//d -= 9; // d=d - 9;
+	//d *= 19; // d=d * 9;
+	//std::cout << d << std::endl;
+
+	//int z = 5, l = 9, m = -27;
+
+      int a = 5;
+	a += 5; //a = a + 5;
+
+	int b = -5;
+	a = -b; // a = +5
+	//a =+ b means a = +b means a=b;
+
+	std::cout << a << std::endl;
+
+	//(tip_data_conversie) variabila
+
+	float numar = (float)10 / (float)3;
+	int var1 = (int)numar;
+
+
+
+
+
+
+
+
+
+
+	
+	
+
+
+
+	
+
+
+
+}
+
